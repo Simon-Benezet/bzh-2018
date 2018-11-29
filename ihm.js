af966d06f6785077a02bcd09fbc6b99502a3c151
Simon-Benezet <simon.benezet@hotmail.fr>
2018-11-29 12:41:33 +0100

    +

--- ihm.js
+++ ihm.js
@@ -1,35 +1,34 @@
-var service = require('./service')
-var readline = require('readline');
+const service = require('./service')
+const readline = require('readline');
 exports.start = function () {
     Menu()
     function Menu() {
-        console.log('*************************')
+        console.log('_________________________')
         console.log('1. Rafraichir les données');
         console.log('2. Lister les sessions')
         console.log('99. Quitter')
 
-        var menu = readline.createInterface({
+        let menu = readline.createInterface({
             input: process.stdin,
             output: process.stdout
         })
 
-        menu.question("\nChoisissez une option : ", function (saisie) {
+        menu.question("\nChoisissez une option : ", saisie => {
             if (`${saisie}` == 1) {
-                service.init(function (nb) {
-                    console.log('[init]', nb, 'sessions trouvées.', '\n...Données mises à jour')
+                service.init(nb => {
+                    console.log(`[init] ${nb} sessions trouvées.\n...Données mises à jour`)
                 });
                 menu.close();
             }
 
             else if (`${saisie}` == 2) {
-                service.listerSessions(function () {
-                    console.log('[init]', nb, 'sessions trouvées.')
-                })
+                service.listerSessions()
                 menu.close();
             }
 
-            if (`${saisie}` != 99) {
+            if (`${saisie}` == 99) {
                 menu.close();
+                return console.log("vous avez quitté");
             }
             Menu()
         })
--- service.js
+++ service.js
@@ -1,22 +1,50 @@
 //tableau qui contendra toutes les sessions du BreizhCamp
-var talks = [];
-var request = require('request');
+let talks = [];
+const request = require('request-promise-native');
 
-exports.init = function (callback) {
-    request('http://2018.breizhcamp.org/json/talks.json', { json: true }, function (err, res, body) {
-        if (err) { return console.log('Erreur', err); }
-        talks = body;
-        request('http://2018.breizhcamp.org/json/others.json', { json: true }, function (err, res, body2) {
-            if (err) { return console.log('Erreur', err); }
-            talks = talks.concat(body2);
-            callback(talks.length);
-            return talks;
-        });
-    });
+// exports.init = callback => {
+//     request('http://2018.breizhcamp.org/json/talks.json', { json: true }, (err, res, body) => {
+//         if (err) { return console.log(`Erreur : ${err}`); }
+//         talks = body;
+//         request('http://2018.breizhcamp.org/json/others.json', { json: true }, (err, res, body2) => {
+//             if (err) { return console.log(`Erreur : ${err}`); }
+//             talks = talks.concat(body2);
+//             callback(talks.length);
+//             return talks;
+//         });
+//     });
+// }
+
+exports.init = (nb) => {
+    return new Promise((resolve, reject) => {
+        request('http://2018.breizhcamp.org/json/talks.json',
+            { json: true }, (err, res, body) => {
+                talks = body;
+                
+                request('http://2018.breizhcamp.org/json/others.json',
+                    { json: true }, (err, res, body2) => {
+                        talks = talks.concat(body2);
+                        if (err) {
+                            reject(err)
+                        } else {
+                            nb = talks.length
+                            resolve(nb)
+                        }
+                    })
+            })
+    })
 }
 
-exports.listerSessions = function () {
-    talks.forEach(function (element) {
-        console.log(element.name, "(Speakers :", element.speakers + ")\n");
-    });
+
+exports.listerSessions = () => {
+    return new Promise((resolve, reject) => {
+        if (false) {
+            reject("les données n'ont pas été récupéré");
+        } else {
+            resolve(talks.forEach(element => {
+                console.log(`${element.name}, (Speakers : ${element.speakers})\n`);
+            }))
+        }
+
+    })
 }
